8834d96dd030181ff86a90ed95761743d1586c69
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-07-01 12:36:24 +0500

    complete responsive footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,26 @@
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import Image from "next/image";
-import Logo from '../../../public/assets/Company.svg'
+import Logo from '/public/assets/Company.svg'
 
 const Footer = () => {
+    {/* I can use Grid layout for footer in mobile display then adjust the y of every element in grid */ }
     return (
         <footer className="xl:pb-7 xl:pt-[17px] px-8 md:px-0 py-[17px] md:mx-[62px] xl:mx-[70.5px]">
             {/* Upper Part */}
-            <div className="flex justify-between py-[10px]">
+            <div className="flex flex-col mb-10 sm:mb-0 sm:flex-row gap-5 sm:gap-0 justify-between sm:py-2.5">
                 <div>
                     <Image src={Logo} alt="Logo" />
                 </div>
-                <div className="flex gap-8 text-gray-600 items-center">
+
+                {/* visible in moblie only */}
+                <div className="sm:hidden text-gray-600">
+                    <p className="text-sm">
+                        Crafting Connections,<br /> One Customer at a Time.
+                    </p>
+                </div>
+
+                {/* hidden in mobile view */}
+                <div className="hidden sm:flex gap-8 text-gray-600 items-center">
                     <div>
                         <a href="#" className="block text-sm hover:text-gray-800">Dashboard</a>
                     </div>
@@ -29,11 +39,31 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col md:flex-row justify-between items-center md:mb-6 md:mt-[14px] xl:my-4">
-                <div className="text-gray-600">
+            <div className="flex flex-col sm:flex-row justify-between gap-10 sm:items-center md:mb-6 md:mt-3.5 xl:my-4">
+                {/* hidden in moblie view */}
+                <div className="hidden sm:block sm:text-gray-600">
                     <p className="text-sm">
                         Crafting Connections, One Customer at a Time.
                     </p>
+                </div>
+
+                {/* visible in mobile view */}
+                <div className="sm:hidden flex flex-col gap-6 text-gray-600 items-left">
+                    <div>
+                        <a href="#" className="block text-sm hover:text-gray-800">Dashboard</a>
+                    </div>
+                    <div>
+                        <a href="#" className="block text-sm hover:text-gray-800">Customers</a>
+                    </div>
+                    <div>
+                        <a href="#" className="block text-sm hover:text-gray-800">Order Overview</a>
+                    </div>
+                    <div>
+                        <a href="#" className="block text-sm hover:text-gray-800">Analytics</a>
+                    </div>
+                    <div>
+                        <a href="#" className="block text-sm hover:text-gray-800">Accounting</a>
+                    </div>
                 </div>
 
                 <div className="flex space-x-4">
@@ -49,11 +79,11 @@ const Footer = () => {
                 </div>
             </div>
             {/* Lower Part */}
-            <div className="flex justify-between items-center border-t-2 md:pt-[22px] xl:pt-[30px] border-gray-200">
+            <div className="flex flex-col justify-between gap-3 sm:gap-0 sm:items-center sm:flex-row sm:border-t-2 my-10 sm:my-0 sm:pt-[22px] xl:pt-[30px] border-gray-200">
                 <div className="text-gray-600 text-sm py-[5px]">
                     <p>Privacy Policy</p>
                 </div>
-                <div className="text-gray-600 text-sm py-[5px]">
+                <div className="order-last sm:order-none text-gray-600 text-sm sm:py-[5px]">
                     <p>&copy; 2023 Mugna Technologies, Inc.</p>
                 </div>
                 <div className="text-gray-600 text-sm py-[5px]">
